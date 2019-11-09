53f9df894b7c1dc6172e12bf5746aec6c63134c5
linyongsyuan <herewego8520@outlook.com>
2019-11-09 13:50:50 +0800

    Initial commit

---
+++ src/main/java/com/tom/Ggs.java
@@ -0,0 +1,11 @@
+package com.tom;
+
+public class Ggs {
+    public void Apple(){
+        System.out.println("APPLE");
+    }
+    public void Bn(){
+        System.out.println("Banana");
+    }
+
+}
---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,26 @@
+package com.tom;
+
+public class Hello {
+    public static void main(String[] args) {
+    //    System.out.println("hello");
+
+    //new Person().hello();
+    Person p = new Person();
+    p.hello();
+    int age = 19;
+    Integer age2 = 19;
+
+    char c = '我';
+   Character c2='A';
+    byte b = 120;
+    float weight = 66.5f;
+    float height = 1.7f;
+    boolean adult = true;
+    boolean enroll = false;
+    String name = "Tome";
+
+    }
+
+
+
+}
